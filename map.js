a610a4fb83ba90e74194ec36cf04c8c24cee22fe
manuligit <riina.vaisanen@gmail.com>
2018-11-03 16:29:13 +0200

    Markers can be added for each place

--- map.js
+++ map.js
@@ -1,9 +1,39 @@
 var map;
+var markers = [];
+
+// Create dummy data to test marking on the map: in real app, read data from backend here
+const place1 = {
+  title: "Kasinopuisto",
+  desc: "A nice park with nice scenery",
+  coords: {lat: 60.148500, lng: 24.987445},
+  opentime: "0000",
+  closetime: "2400",
+}
+  
+const place2 = {
+  title: "Suomenlinnan kirjasto",
+  desc: "Librarby",
+  coords: {lat: 60.1462116, lng: 24.9846877},
+  opentime: "1300",
+  closetime: "1915",
+}
+
+let places = [place1, place2];
+
+// Initialize the map:
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 60.147497, lng: 24.988798},
     zoom: 16
   });
 
-  console.log(document.getElementById('map'))
+  places.forEach(place => {
+    var marker = new google.maps.Marker({
+      position: place.coords,
+      map: map,
+      title: place.title,
+    });
+    markers.push(marker);
+  });
 }
---
+++ script.js
@@ -0,0 +1,15 @@
+// const place1 = {
+//   title: "Kasinopuisto",
+//   desc: "A nice park with nice scenery",
+//   coords: [60.148500, 24.987445],
+//   opentime: "0000",
+//   closetime: "2400",
+// }
+  
+// const place2 = {
+//   title: "Suomenlinnan kirjasto",
+//   desc: "Librarby",
+//   coords: [60.1462116,24.9846877],
+//   opentime: "1300",
+//   closetime: "1915",
+// }
